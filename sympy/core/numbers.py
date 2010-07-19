fe0690d3e3e971e4677b282078c24aab212ed88b
Mateusz Paprocki <mattpap@gmail.com>
2010-07-19 15:46:29 +0200

    Implemented sqrt() method in Integer class

--- sympy/core/numbers.py
+++ sympy/core/numbers.py
@@ -1110,6 +1110,10 @@ class Integer(Rational):
     def __rfloordiv__(self, other):
         return Integer(Integer(other).p // self.p)
 
+    def sqrt(a):
+        """Compute integer square root of `a`. """
+        return Integer(mlib.isqrt(int(a)))
+
     def half_gcdex(a, b):
         """Half Extended Euclidean Algorithm. """
         s, _, h = a.gcdex(b)
@@ -1147,7 +1151,7 @@ class Integer(Rational):
             raise ZeroDivisionError("zero divisor")
 
     def cofactors(a, b):
-        """Returns GCD and cofactors of input arguments. """
+        """Compute GCD and cofactors of input arguments. """
         if type(b) is int:
             gcd = Integer(igcd(int(a), b))
             return gcd, a//gcd, Integer(b)//gcd
@@ -1161,7 +1165,7 @@ class Integer(Rational):
                 raise ValueError("expected an integer, got %s" % b)
 
     def gcd(a, b):
-        """Returns greates common divisor of input arguments. """
+        """Compute greatest common divisor of input arguments. """
         if type(b) is int:
             return Integer(igcd(int(a), b))
         else:
@@ -1173,7 +1177,7 @@ class Integer(Rational):
                 raise ValueError("expected an integer, got %s" % b)
 
     def lcm(a, b):
-        """Returns least common multiple of input arguments. """
+        """Compute least common multiple of input arguments. """
         if type(b) is int:
             return Integer(ilcm(int(a), b))
         else:
@@ -1186,7 +1190,6 @@ class Integer(Rational):
 
 # Add sympify converters
 converter[int] = converter[long] = Integer
-
 
 class Zero(Integer):
     __metaclass__ = SingletonMeta
